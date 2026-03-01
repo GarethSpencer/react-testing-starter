c39853e3d645df58ae516fdbcd6337202c06e083
Gareth Spencer <gareth.spencer1@hotmail.co.uk>
2026-03-01 11:32:18 +0000

    adding mock service worker

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,5 +1,10 @@
 import "@testing-library/jest-dom/vitest";
 import ResizeObserver from "resize-observer-polyfill";
+import { server } from "./mocks/server";
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 globalThis.ResizeObserver = ResizeObserver;
 
@@ -9,14 +14,16 @@ window.HTMLElement.prototype.releasePointerCapture = vi.fn();
 
 Object.defineProperty(window, "matchMedia", {
   writable: true,
-  value: vi.fn().mockImplementation((query) => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: vi.fn(), // deprecated
-    removeListener: vi.fn(), // deprecated
-    addEventListener: vi.fn(),
-    removeEventListener: vi.fn(),
-    dispatchEvent: vi.fn(),
-  })),
+  value: vi.fn().mockImplementation(
+    (query: string): MediaQueryList => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: vi.fn(), // deprecated
+      removeListener: vi.fn(), // deprecated
+      addEventListener: vi.fn(),
+      removeEventListener: vi.fn(),
+      dispatchEvent: vi.fn(),
+    }),
+  ),
 });
